5e307f51e656ccc5f510a137bbd6dc40564a701a
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-30 07:18:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,6 +50,10 @@ class Student
     DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT (?)", n).map { |row| self.new_from_db(row) }
   end
 
+  def self.first_student_in_grade_10
+    DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT 1").map { |row| self.new_from_db(row) }
+  end
+
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
