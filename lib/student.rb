930113e4b26f8719fdaf958e07f8c522c0a3c4f5
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-30 07:19:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT 1").map { |row| self.new_from_db(row) }
+    DB[:conn].execute("SELECT * FROM students WHERE grade = 10 ASC LIMIT 1").map { |row| self.new_from_db(row) }
   end
 
   def self.drop_table
