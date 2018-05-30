db9ecceba4cff8a1bfd0d50ca650a047094ec16a
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-30 07:00:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ? LIMIT(1)"
-    DB[:conn].execute(sql).find { |row| self.new_from_db(row) }
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
   end
 
   def save
