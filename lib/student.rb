2a18e2c5190bf74bc6f28e72b23cfe02ade1324f
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-30 07:17:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,7 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(n)
-    DB[:conn].execute("SELECT * FROM students WHERE grade = ?, LIMIT (?)", 10, n).map { |row| self.new_from_db(row) }
+    DB[:conn].execute("SELECT * FROM students WHERE grade = ? LIMIT (?)", 10, n).map { |row| self.new_from_db(row) }
   end
 
   def self.drop_table
